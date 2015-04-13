91e74950ec12d8bd657d977ac5291c491560867d
gessinger-hj <gessinger.hj@gmail.com>
2015-04-13 19:11:36 +0200

    Py 3

--- python/Gepard.py
+++ python/Gepard.py
@@ -2,30 +2,53 @@
 
 from cStringIO import StringIO
 import json
-import jsonpickle
+import inspect
+import time
+import datetime
 
 class Event ( object ):
-	def __init__ (self,name,type=None):
+	def __init__ (self,name,type=None,body=None):
+		if not isinstance ( name, basestring):
+			raise ValueError ( "name must be a non empty string, not: " + str(name) + "(" + name.__class__.__name__ + ")" )
+		if type != None and not isinstance ( type, basestring):
+			raise ValueError ( "type must be None or a non empty string, not: " + str(type) + "(" + type.__class__.__name__ + ")" )
 		self.name = name
 		self.type = type
-		self.body = { "a": 1 }
+		if body == None:
+			self.body = {}
+		elif isinstance ( body, dict ):
+			self.body = body
+		else:
+			raise ValueError ( "body must be None or a dict, not: " + str(body) + "(" + name.__class__.__name__ + ")" )
+		self.user = None
+		self.control = { "createdAt": datetime.datetime.now().replace(microsecond=0) } ;
+	def getName ( self ):
+		return self.name
 	def __str__(self):
 		str = StringIO()
 		str.write("(")
 		str.write(self.__class__.__name__)
 		str.write(")")
 		return str.getvalue()
-	# def __repr__(self):
-		# return '[{ "type":"Event", "value":{ "name":"' + repr(self.name) + '", "type":"' + repr(self.type) + '"}}]'
-		# return '{"s": "instance value goes here", "__module__": "json_myobj", "__class__": "MyObj"}'
-		# return "1"
-x = 1
-if x == 1:
-  # indented four spaces
-	print "x is 1."
 
-import pprint
+e = Event ("ALARM")
+print datetime.datetime.now().replace(microsecond=0) #.isoformat()
+print e.control["createdAt"]
 
+def to_json(obj):
+	if isinstance ( obj, bytes ):
+		return { '__class__': 'bytes',
+						 '__value__': list(obj)
+					 }
+	if isinstance ( obj, Event ):
+		return { '__class__': 'Event',
+						 '__value__': { 'name':obj.name, "type":obj.type }
+					 }
+	raise TypeError ( repr(obj) + ' is not JSON serializable' )
+
+print json.dumps ( e, default=to_json, indent=2 )
+print dir(e)
+# print inspect.getmembers ( e )
 # def convert_to_builtin_type(obj):
 #     print 'default(', repr(obj), ')'
 #     # Convert objects to a dictionary of their representation
@@ -35,18 +58,4 @@ import pprint
 #     d.update(obj.__dict__)
 #     return d
 
-e = Event("ALARM")
-print e
-print e.__dict__
-
-print repr(e.__dict__)
-# print repr(e)
-print json.dumps ( e.__dict__ )
-print json.dumps ( e )
-# json.encode ( e )
-# pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(e)
-# print pp.pformat(e)
-
-"""
-"""
+# print json.dumps ( e )
