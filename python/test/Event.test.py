29513d5dfb49bfb0864d3292d5bc661b9dd06585
Hans Jürgen Gessinger <gessinger.hj@gmail.com>
2016-01-03 23:49:52 +0100

    JSAcc .js .py .java

---
+++ python/test/Event.test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os, sys, datetime
+sys.path.insert ( 0, os.path.dirname(os.path.abspath(__file__) ) + "/../" )
+
+import gepard
+
+import json
+import threading
+import time
+import types
+import time
+
+try:
+	from cStringIO import StringIO
+except ImportError:
+	from io import StringIO
+
+# import ipaddress
+# ==========================================================================
+
+from glob import glob
+
+e = gepard.Event ( "__FILE__" )
+
+# e.setUser ( gepard.User ( "gess" ) )
+
+e.putValue ( "STRING", "TEXT" )
+e.putValue ( "BINARY", bytearray ( [ 64, 65, 66, 67 ] ) )
+e.putValue ( "DATE", datetime.datetime.now() )
+e.putValue ( "STRING/IN/PATH", "AAA" )
+e.putValue ( "STRING/IN/PATH2", "BBB" )
+e.setStatus ( 0, "success", "File accepted.")
+e.setIsResult()
+print ( e.getStatus() )
+print ( e.getStatusReason() )
+print ( e.getStatusName() )
+print ( e.getStatusCode() )
+print ( e.isBad() )
+print ( "e.isResult()=" + str ( e.isResult() ) )
+
+
+t = e.serialize()
+print ( t )
+ee = gepard.Event.deserialize ( t )
+print ( e.getStatus() )
+print ( ee )
+
---
+++ python/test/JSAcc.test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os, sys
+sys.path.insert ( 0, os.path.dirname(os.path.abspath(__file__) ) + "/../" )
+
+import gepard
+
+import json
+import threading
+import time
+import types
+import time
+
+try:
+	from cStringIO import StringIO
+except ImportError:
+	from io import StringIO
+
+from glob import glob
+
+a = gepard.JSAcc()
+a.add ( "M1/M2/N", 11 ) ;
+# print ( '1 ----------------------' ) ;
+print ( json.dumps ( a.value ( "M1/M2/N" ) ) ) ;
+print ( json.dumps ( a.value ( "M1/M2" ) ) ) ;
+a.add ( "A/B/C", [ "ABCD", "ABCE" ] ) ;
+a.add ( "A/B/D", "ABCD" ) ;
+a.add ( "A/AX", "AX" ) ;
+a.add ( "X", "X" ) ;
+# print ( '2 ----------------------' ) ;
+print ( json.dumps ( a.map ) ) ;
+# print ( '3 ----------------------' ) ;
+print ( json.dumps ( a.value ( "A/B" ) ) ) ;
+a.remove ( "X" ) ;
+# print ( '4 -- a.remove ( "X" )' ) ;
+print ( json.dumps ( a.map ) ) ;
+a.remove ( "A/B/D" ) ;
+# print ( '5 -- a.remove ( "A/B/D" )' ) ;
+print ( json.dumps ( a.map ) ) ;
+a.remove ( "A" ) ;
+# print ( '6 -- a.remove ( "A" )' ) ;
+print ( json.dumps ( a.map ) ) ;
+a.remove ( "M1/M2/N" ) ;
+# print ( '7 -- a.remove ( "M1/M2/N" )' ) ;
+print ( json.dumps ( a.map ) ) ;
