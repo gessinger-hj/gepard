0d4c59b00c446f705c2c619aa9c2d4fe4e78ed03
gessinger-hj <gessinger.hj@gmail.com>
2016-02-26 20:20:18 +0100

    Configurable Zeroconf

--- java/src/org/gessinger/gepard/zeroconf/MDNSLookup.java
+++ java/src/org/gessinger/gepard/zeroconf/MDNSLookup.java
@@ -19,10 +19,6 @@ package org.gessinger.gepard.zeroconf ;
 
 import java.io.IOException;
 import java.util.Enumeration;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
@@ -40,19 +36,25 @@ public class MDNSLookup
   public static void main ( String[] args )
   {
     Util.argsToProperties ( args ) ;
-    /* Activate these lines to see log messages of JmDNS */
-    boolean log = false;
-    if (log) {
-        ConsoleHandler handler = new ConsoleHandler();
-        handler.setLevel(Level.FINEST);
-        for (Enumeration<String> enumerator = LogManager.getLogManager().getLoggerNames(); enumerator.hasMoreElements();) {
-            String loggerName = enumerator.nextElement();
-            Logger logger = Logger.getLogger(loggerName);
-            logger.addHandler(handler);
-            logger.setLevel(Level.FINEST);
-        }
+    try
+    {
+      MDNSLookup ml = new MDNSLookup() ;
+      ml.monitor() ;
     }
-
+    catch ( Exception exc )
+    {
+      System.out.println ( Util.toString ( exc ) ) ;
+    }
+  }
+  public MDNSLookup (  )
+  throws Exception
+  {
+    
+  }
+  public void monitor (  )
+  throws Exception
+  {
+    String type = Util.getProperty ( "type", "gepard" ) ;
     JmDNS jmdns = null;
     try
     {
@@ -60,47 +62,50 @@ public class MDNSLookup
       while ( true )
       {
         // String type = "_airport._tcp.local." ;
-        String type = "_test-gepard._tcp.local." ;
-        ServiceInfo[] infos = jmdns.list ( type ) ;
-        System.out.println("List " + type );
-        for (int i = 0; i < infos.length; i++) {
-            System.out.println(infos[i]);
-            // System.out.println ( "infos[i].getType()=" + infos[i].getType() ) ;
-            // System.out.println ( "infos[i].getTypeWithSubtype()=" + infos[i].getTypeWithSubtype() ) ;
-            // System.out.println ( "infos[i].getName()=" + infos[i].getName() ) ;
-            // System.out.println ( "infos[i].getKey()=" + infos[i].getKey() ) ;
-            // System.out.println ( "infos[i].getQualifiedName()=" + infos[i].getQualifiedName() ) ;
-            System.out.println ( "infos[i].getServer()=" + infos[i].getServer() ) ;
-            System.out.println ( "infos[i].getHostAddress()=" + infos[i].getHostAddress() ) ;
-            System.out.println ( "infos[i].getAddress()=" + infos[i].getAddress() ) ;
-            // System.out.println ( "infos[i].getInetAddress()=" + infos[i].getInetAddress() ) ;
-            // System.out.println ( "infos[i].getInet4Address()=" + infos[i].getInet4Address() ) ;
-            // System.out.println ( "infos[i].getInet6Address()=" + infos[i].getInet6Address() ) ;
-            System.out.println ( "infos[i].getPort()=" + infos[i].getPort() ) ;
-            // System.out.println ( "infos[i].getPriority()=" + infos[i].getPriority() ) ;
-            // System.out.println ( "infos[i].getWeight()=" + infos[i].getWeight() ) ;
-            // System.out.println ( "infos[i].getTextString()=" + infos[i].getTextString() ) ;
-            // System.out.println ( "infos[i].getURL()=" + infos[i].getURL() ) ;
-            // // System.out.println ( "infos[i].getPropertyString()=" + infos[i].getPropertyString() ) ;
-            // System.out.println ( "infos[i].getNiceTextString()=" + infos[i].getNiceTextString() ) ;
-            // System.out.println ( "infos[i].isPersistent()=" + infos[i].isPersistent() ) ;
-            // System.out.println ( "infos[i].getProtocol()=" + infos[i].getProtocol() ) ;
-            // System.out.println ( "infos[i].getApplication()=" + infos[i].getApplication() ) ;
-
-        if ( infos[i].getTextBytes().length > 0 )
+        String fullType = "_" + type + "._tcp.local." ;
+        ServiceInfo[] infos = jmdns.list ( fullType ) ;
+        System.out.println("List " + fullType );
+        for (int i = 0; i < infos.length; i++)
         {
-          StringBuilder buf = new StringBuilder() ;
-            // buf.append("\n");
-            buf.append(infos[i].getNiceTextString());
-            buf.append("\n");
-            buf.append(infos[i].getTextString());
-            buf.append("\n");
-            for ( Enumeration<String> en = infos[i].getPropertyNames() ; en.hasMoreElements() ; )
-            {
-              String key = en.nextElement() ;
-              buf.append("\n");
-              buf.append ("\t" + key + ": " + new String ( infos[i].getPropertyString ( key ) ) + "\n" ) ;
-            }
+          String fqdn = infos[i].getName() + "." + infos[i].getType() ;
+          System.out.println ( fqdn ) ;
+          System.out.println ( "infos[i].getPort()=" + infos[i].getPort() ) ;
+          // System.out.println ( "infos[i].getType()=" + infos[i].getType() ) ;
+          // System.out.println ( "infos[i].getTypeWithSubtype()=" + infos[i].getTypeWithSubtype() ) ;
+          // System.out.println ( "infos[i].getName()=" + infos[i].getName() ) ;
+          // System.out.println ( "infos[i].getKey()=" + infos[i].getKey() ) ;
+          // System.out.println ( "infos[i].getQualifiedName()=" + infos[i].getQualifiedName() ) ;
+          // System.out.println ( "infos[i].getServer()=" + infos[i].getServer() ) ;
+          // System.out.println ( "infos[i].getHostAddress()=" + infos[i].getHostAddress() ) ;
+          // System.out.println ( "infos[i].getAddress()=" + infos[i].getAddress() ) ;
+          // System.out.println ( "infos[i].getInetAddress()=" + infos[i].getInetAddress() ) ;
+          // System.out.println ( "infos[i].getInet4Address()=" + infos[i].getInet4Address() ) ;
+          // System.out.println ( "infos[i].getInet6Address()=" + infos[i].getInet6Address() ) ;
+          // System.out.println ( "infos[i].getPort()=" + infos[i].getPort() ) ;
+          // System.out.println ( "infos[i].getPriority()=" + infos[i].getPriority() ) ;
+          // System.out.println ( "infos[i].getWeight()=" + infos[i].getWeight() ) ;
+          // System.out.println ( "infos[i].getTextString()=" + infos[i].getTextString() ) ;
+          // System.out.println ( "infos[i].getURL()=" + infos[i].getURL() ) ;
+          // // System.out.println ( "infos[i].getPropertyString()=" + infos[i].getPropertyString() ) ;
+          // System.out.println ( "infos[i].getNiceTextString()=" + infos[i].getNiceTextString() ) ;
+          // System.out.println ( "infos[i].isPersistent()=" + infos[i].isPersistent() ) ;
+          // System.out.println ( "infos[i].getProtocol()=" + infos[i].getProtocol() ) ;
+          // System.out.println ( "infos[i].getApplication()=" + infos[i].getApplication() ) ;
+
+          // if ( infos[i].getTextBytes().length > 0 )
+          // {
+          //   StringBuilder buf = new StringBuilder() ;
+          //     // buf.append("\n");
+          //     buf.append(infos[i].getNiceTextString());
+          //     buf.append("\n");
+          //     buf.append(infos[i].getTextString());
+          //     buf.append("\n");
+          //     for ( Enumeration<String> en = infos[i].getPropertyNames() ; en.hasMoreElements() ; )
+          //     {
+          //       String key = en.nextElement() ;
+          //       buf.append("\n");
+          //       buf.append ("\t" + key + ": " + new String ( infos[i].getPropertyString ( key ) ) + "\n" ) ;
+          //     }
           // Map<String, byte[]> properties = this.getProperties();
           // if (!properties.isEmpty()) {
           //     buf.append("\n");
@@ -110,13 +115,11 @@ public class MDNSLookup
           // } else {
           //     buf.append(" empty");
           // }
-          System.out.println ( buf ) ;
+          // System.out.println ( buf ) ;
         }
+        System.out.println ( "--------------" ) ;
 
-        }
-        System.out.println();
-
-        Thread.sleep(5000);
+        Thread.sleep(10000);
       }
     }
     catch ( Exception e )
--- xmp/ZeroconfListener.js
+++ xmp/ZeroconfListener.js
@@ -4,15 +4,6 @@ if ( require.main === module )
 {
 	var gepard = require ( "gepard" ) ;
 
-	if ( gepard.getProperty ( "help" ) )
-	{
-		console.log (
-			"Gepard Examples: Listener, listen to a given event.\n"
-		+ "Usage: node Listener [Options]\n"
-		+ "Options: -Dname=<event-name>, default <event-name>=ALARM,BLARM\n"
-		) ;
-		process.exit() ;
-	}
 	var name = gepard.getProperty ( "name", "ALARM,BLARM" ) ;
 	if ( name.indexOf ( ',' ) > 0 )
 	{
@@ -25,11 +16,11 @@ if ( require.main === module )
 	var client = gepard.getClient ( { type: 'test-gepard' }, function acceptService ( service, isReconnect )
 	{
 		console.log ( service.txt ) ;
-		console.log ( "Listen for events with name=" + name ) ;
-		if ( ! isReconnect )
-		{
-			client.on ( name, (e) => { console.log ( e ) ; } ) ;
-		}
 		return true ;
 	} ) ;
+	client._on_ ( "connect" ) ;
+	client.onConnect ( () => {
+		console.log ( "Listen for events with name=" + name ) ;
+		client.on ( name, (e) => { console.log ( e ) ; } ) ;
+	});
 }
