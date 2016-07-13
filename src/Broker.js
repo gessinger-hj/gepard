05d73ebf67fc494fda7136bccaf4bcf66eb93031
Hans Jürgen Gessinger <gessinger.hj@gmail.com>
2016-07-13 15:50:35 +0200

    setConfig() if Broker is instantiated from a JS script

--- src/Broker.js
+++ src/Broker.js
@@ -390,6 +390,7 @@ TPStore.add ( "EVENT_OUT" ) ;
 var Broker = function ( port, ip )
 {
   EventEmitter.call ( this ) ;
+  this._configIsSet = false ;
   this._connections                        = {} ;
   this._eventNameToSockets                 = new MultiHash() ;
   this._channelNameToSockets               = new MultiHash() ;
@@ -1531,6 +1532,11 @@ Broker.prototype._closeAllSockets = function ( exceptSocket )
  */
 Broker.prototype.listen = function ( port, callback )
 {
+  if ( !this._configIsSet )
+  {
+    this.setConfig() ;
+  }
+
   if ( port ) this.port = port ;
   if ( ! this.port )
   {
@@ -1738,6 +1744,7 @@ Broker.prototype.unpublishService = function()
  */
 Broker.prototype.setConfig = function ( configuration )
 {
+  this._configIsSet = true ;
   var connectionHookIsFile = false ;
   var hook ;
   if ( ! configuration )
@@ -1899,7 +1906,6 @@ if ( require.main === module )
     Log.init ( "level=info,Xedirect=3,file=%GEPARD_LOG%/%APPNAME%.log:max=1m:v=4") ;
 
     var b = new Broker() ;
-    b.setConfig() ;
     b.listen() ;
     var wse ;
     b.on ( "shutdown", function onshutdown(e)
